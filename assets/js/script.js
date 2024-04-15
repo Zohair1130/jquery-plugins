367f7c6029534d32e23011b75fd8cb36a9881399
Zohair1130 <kamranz1@tcnj.edu>
2024-04-15 16:07:31 -0400

    class progress

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+console.log("hello")
+
+if(jQuery) {
+    console.log("jquery loaded")
+}
+
+else {
+    console.log("no jquery")
+}
+
+
+let mobileMenuOpen = false;
+
+$("#mobile-menu-button").click(function(){
+    // if mobile menu is closed, open it
+    if(mobileMenuOpen == false) {
+
+    $("#main-menu").removeClass("display-none-mobile");
+    mobileMenuOpen = true;
+    $("#mobile-menu-text").text("Close");
+    $("#mobile-menu-icon").attr("src", "assets/images/close.svg");
+    }
+    // if mobile menu is closed, open it
+    else {
+
+    $("#main-menu").addClass("display-none-mobile");
+    mobileMenuOpen = false;
+    $("#mobile-menu-text").text("Menu");
+    $("#mobile-menu-icon").attr("src", "assets/images/menu.svg");
+    }
+});
